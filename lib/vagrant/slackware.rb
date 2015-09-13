f0dd28d3c6a082f4e92cbbf434319aee7c398c42
Mikko Värri <vmj@linuxbox.fi>
2015-09-13 17:20:22 +0300

    bundle gem vagrant-slackware

---
+++ lib/vagrant/slackware.rb
@@ -0,0 +1,7 @@
+require "vagrant/slackware/version"
+
+module Vagrant
+  module Slackware
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/slackware/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Slackware
+    VERSION = "0.1.0"
+  end
+end
